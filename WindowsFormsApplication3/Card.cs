4f8d89c3a79f815d01508b18f0ed26a741e72d5d
Unknown <mkaufmann421@gmail.com>
2017-09-22 09:46:32 -0400

    Added initial files
    First commit

---
+++ WindowsFormsApplication3/Card.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Card
+{
+    public enum colors
+    {
+        red,
+        green,
+        blue
+    };
+    public enum patterns
+    {
+        solid,
+        striped,
+        empty
+    };
+    public enum shapes
+    {
+        circle,
+        square,
+        star
+    }
+
+    public colors cardColor;
+    public int cardNum;
+    public patterns cardPattern;
+    public shapes cardShape;
+    public Card(colors color, int num, patterns pattern, shapes shape)
+    {
+        cardColor = color;
+        cardNum = num;
+        cardPattern = pattern;
+        cardShape = shape;
+    }
+}
---
+++ WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            List<PictureBox> boxes = new List<PictureBox>();
+            boxes.AddRange(new PictureBox[]{ pictureBox1, pictureBox2, pictureBox3, pictureBox4, pictureBox5, pictureBox6, pictureBox7, pictureBox8, pictureBox9, pictureBox10, pictureBox11, pictureBox12});
+            List<Image> images = new List<Image>();
+            string fileName = "";
+            string color = "";
+            string pattern ="";
+            string shape="";
+            for (int i = 0; i < 3; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        color = "r";
+                        break;
+                    case 1:
+                        color = "g";
+                        break;
+                    case 2:
+                        color = "b";
+                        break;
+                }
+                for (int j = 0; j < 3; j++)
+                {
+                    switch (j)
+                    {
+                        case 0:
+                            pattern = "empty";
+                            break;
+                        case 1:
+                            pattern = "solid";
+                            break;
+                        case 2:
+                            pattern = "striped";
+                            break;
+                    }
+                    for (int k = 0; k < 3; k++)
+                    {
+                        switch (k)
+                        {
+                            case 0:
+                                shape = "circle";
+                                break;
+                            case 1:
+                                shape = "square";
+                                break;
+                            case 2:
+                                shape = "star";
+                                break;
+                        }
+                        for (int l = 1; l < 4; l++)
+                        {
+                            fileName = color + pattern + shape + l.ToString();
+                            images.Add(Image.FromFile(fileName));
+                        }
+                    }
+                }
+            }
+        }
+
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
