68df889018ff4f18a0280609e581450648daaf2f
Unknown <mkaufmann421@gmail.com>
2017-09-24 16:29:36 -0400

    Made gameplay

--- WindowsFormsApplication3/Form1.Designer.cs
+++ WindowsFormsApplication3/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
@@ -40,6 +41,8 @@
             this.pictureBox10 = new System.Windows.Forms.PictureBox();
             this.pictureBox11 = new System.Windows.Forms.PictureBox();
             this.pictureBox12 = new System.Windows.Forms.PictureBox();
+            this.SetsLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -162,11 +165,32 @@
             this.pictureBox12.TabStop = false;
             this.pictureBox12.Click += new System.EventHandler(this.pictureBox_Click);
             // 
+            // SetsLabel
+            // 
+            this.SetsLabel.AutoSize = true;
+            this.SetsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SetsLabel.Location = new System.Drawing.Point(12, 464);
+            this.SetsLabel.Name = "SetsLabel";
+            this.SetsLabel.Size = new System.Drawing.Size(516, 73);
+            this.SetsLabel.TabIndex = 12;
+            this.SetsLabel.Text = "Total # of Sets: 0";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label1.Location = new System.Drawing.Point(25, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(430, 295);
+            this.label1.TabIndex = 13;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1384, 561);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SetsLabel);
             this.Controls.Add(this.pictureBox10);
             this.Controls.Add(this.pictureBox11);
             this.Controls.Add(this.pictureBox12);
@@ -196,6 +220,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -213,6 +238,8 @@
         private System.Windows.Forms.PictureBox pictureBox10;
         private System.Windows.Forms.PictureBox pictureBox11;
         private System.Windows.Forms.PictureBox pictureBox12;
+        private System.Windows.Forms.Label SetsLabel;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- WindowsFormsApplication3/Form1.cs
+++ WindowsFormsApplication3/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,11 @@ namespace WindowsFormsApplication3
 
         public List<Card> cardsInDeck;
         public List<Card> cardsInPlay = new List<Card>();
+        public List<Card> selectedCards = new List<Card>();
+        public List<PictureBox> selectedBoxes = new List<PictureBox>();
+        public int totalSets = 0;
+
+        public Random random = new Random();
         public Form1()
         {
             InitializeComponent();
@@ -24,7 +30,6 @@ namespace WindowsFormsApplication3
             boxes.AddRange(new PictureBox[]{ pictureBox1, pictureBox2, pictureBox3, pictureBox4, pictureBox5, pictureBox6, pictureBox7, pictureBox8, pictureBox9, pictureBox10, pictureBox11, pictureBox12});
             allCards = GenerateDeck(loadImages());
             cardsInDeck = allCards;
-            Random random = new Random();
             foreach (PictureBox box in boxes)
             {
                 Card selectedCard = cardsInDeck[random.Next(cardsInDeck.Count())];
@@ -38,12 +43,13 @@ namespace WindowsFormsApplication3
         {
             List<Card> cards = new List<Card>();
             int imageIndex = 0;
-            for (int i = 0; i < 3; i++) {
-                for (int j = 1; j <= 3; j++)
+            for (int i = 0; i < 3; i++)
+            {
+                for (int k = 0; k < 3; k++)
                 {
-                    for (int k = 0; k < 3; k++)
+                    for (int l = 0; l < 3; l++)
                     {
-                        for (int l = 0; l < 3; l++)
+                        for (int j = 1; j < 4; j++)
                         {
                             cards.Add(new Card((Card.colors)i, j, (Card.patterns)k, (Card.shapes)l, images[imageIndex]));
                             imageIndex++;
@@ -112,9 +118,132 @@ namespace WindowsFormsApplication3
             }
             return images;
         }
+        public bool verifySet(List<Card> cards)
+        {
+            List<Card.colors> colors = new List<Card.colors>();
+            List<Card.patterns> patterns = new List<Card.patterns>();
+            List<Card.shapes> shapes = new List<Card.shapes>();
+            List<int> nums = new List<int>();
+            foreach(Card card in cards)
+            {
+                colors.Add(card.cardColor);
+                patterns.Add(card.cardPattern);
+                shapes.Add(card.cardShape);
+                nums.Add(card.cardNum);
+            }
+            /*if (!(colors.Any(o => o != colors[0]) || colors.Distinct().Count() == colors.Count()))
+            {
+                colorLabel.Text = "Color: False";
+            }
+            else
+            {
+                colorLabel.Text = "Color: True";
+            }
+            if (!(shapes.Any(o => o != shapes[0]) || shapes.Distinct().Count() == shapes.Count()))
+            {
+                shapeLabel.Text = "Shape: False";
+            }
+            else
+            {
+                shapeLabel.Text = "Shape: True";
+            }
+            if (!(patterns.Any(o => o != patterns[0]) || patterns.Distinct().Count() == patterns.Count()))
+            {
+                patternLabel.Text = "Pattern: False";
+            }
+            else
+            {
+                patternLabel.Text = "Pattern: True";
+            }
+            if (!(nums.Any(o => o != nums[0]) || nums.Distinct().Count() == nums.Count()))
+            {
+                numLabel.Text = "Number: False";
+            }
+            else
+            {
+                numLabel.Text = "Number: True";
+            }*/
+            if (!(colors.All(o => o == colors[0]) || colors.Distinct().Count() == colors.Count()))
+            {
+                return false;
+            }
+            if (!(shapes.All(o => o == shapes[0]) || shapes.Distinct().Count() == shapes.Count()))
+            {
+                return false;
+            }
+            if (!(patterns.All(o => o == patterns[0]) || patterns.Distinct().Count() == patterns.Count()))
+            {
+                return false;
+            }
+            if (!(nums.All(o => o == nums[0]) || nums.Distinct().Count() == nums.Count()))
+            {
+                return false;
+            }
+            //TODO
+            return true;
+        }
         private void pictureBox_Click(object sender, EventArgs e)//Handles all pictureBox clicks
         {
+            PictureBox box = (PictureBox)sender;
+            Image image = box.Image;
+            Card clickedCard = null;
+            if (selectedBoxes.Contains(box))
+            {
+                selectedBoxes.Remove(box);
+                box.BorderStyle = BorderStyle.None;
+                Card cardToRemove = null;
+                foreach(Card card in selectedCards)
+                {
+                    if (image == card.cardImage)
+                    {
+                        cardToRemove = card;
+                    }
+                }
+                selectedCards.Remove(cardToRemove);
+                return;
+            }
+            box.BorderStyle = BorderStyle.FixedSingle;
+            foreach (Card card in cardsInPlay)
+            {
+                if (card.cardImage == image)
+                {
+                    clickedCard = card;
+                    selectedCards.Add(clickedCard);
+                    selectedBoxes.Add(box);
+                    if(selectedCards.Count() >= 3)
+                    {
+                        bool isSet = verifySet(selectedCards);
+                        if (isSet)
+                        {
+                            totalSets++;
+                            SetsLabel.Text = "Total # of Sets: " + totalSets.ToString();
+                            foreach(PictureBox selectedBox in selectedBoxes)
+                            {
+                                Card pickedCard = cardsInDeck[random.Next(cardsInDeck.Count())];
+                                selectedBox.Image = pickedCard.cardImage;
+                                cardsInDeck.Remove(pickedCard);
+                                cardsInPlay.Add(pickedCard);
+                            }
+                            foreach (Card selectedCard in selectedCards)
+                            {
+                                cardsInDeck.Add(selectedCard);
+                            }
+                        }
+                        foreach(PictureBox selectedBox in selectedBoxes)
+                        {
+                            selectedBox.BorderStyle = BorderStyle.None;
+                        }
+                        
+                        selectedCards.Clear();
+                        selectedBoxes.Clear();
+                    }
+
+                    break;
+                }
+            }
+            
 
         }
+        
     }
 }
